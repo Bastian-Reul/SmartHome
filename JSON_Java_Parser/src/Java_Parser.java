152b7b53e151c2f2bec07c3607cf82de4cae1fa9
lazarusCantini <Bastian.Reul@gmx.de>
2019-07-16 11:30:39 +0200

    Der JSON Parser wird erstmal in Java geschrieben

--- JSON_Java_Parser/src/Java_Parser.java
+++ JSON_Java_Parser/src/Java_Parser.java
@@ -7,24 +7,53 @@ public class Java_Parser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Aktor Relais1 = new Aktor();
-/*
+
 		String JSON_string = "\"_Topic\":\"Test/objects/Aktor1\",\"_status\":false,\"_schaltvorgaenge\":2,\"Adresse\":739,\"_toggle_trigger\":true";
 		char[] JSON_char_Array = JSON_string.toCharArray();
+		String JSON_string_neu = "\"_Topic\":\"Test/objects/Aktor1\",\"_status\":true,\"_schaltvorgaenge\":2,\"Adresse\":739,\"_toggle_trigger\":true\n";
+		char[] JSON_char_Array_neu = JSON_string_neu.toCharArray();
+		
 		
 		for(int i=0; i<JSON_string.length(); i++)
 		{
 			System.out.print(JSON_char_Array[i]);
+			Relais1.Topic[i] = JSON_char_Array[i];
 		}
 		System.out.println(" ");
 		System.out.println("Ende");
-	*/	
+		
 		
 		System.out.print("Der Status von Relais1 ist: ");
 		System.out.println(Relais1.get_status());
 		System.out.println("Relais1 wird eingeschaltet: ");
-		Relais1.schalte_ein();
+		Relais1.schalte(true);
 		System.out.print("Der Status von Relais1 ist: ");
 		System.out.println(Relais1.get_status());
+		Relais1.schalte(true);
+		
+		char[] Relais_Array = Relais1.get_topic();
+		
+		for(int i=0; i<Relais1.get_Topic_Lanege(); i++)
+		{
+			System.out.print(Relais_Array[i]);
+		}
+		System.out.println(" ");
+		System.out.println("MQTT Topic Ende");
+		//Relais_Array[50] = '\n';
+		
+		for(int i=0; i<100; i++)
+		{
+			System.out.print(Relais_Array[i]);
+		}
+		System.out.println(" ");
+		Relais1.JSON_2_Aktor(JSON_char_Array_neu);
+		System.out.println(" ");
+		System.out.println("Das neue Topic: ");
+		char [] neuer_Topic = Relais1.get_topic();
+		for(int i=0; neuer_Topic[i]!='\n'; i++)
+		{
+			System.out.print(neuer_Topic[i]);
+		}
 	}
 
 }
--- JSON_Java_Parser/src/de/extern/Aktor.java
+++ JSON_Java_Parser/src/de/extern/Aktor.java
@@ -3,7 +3,9 @@
 
 public class Aktor {
 
-	//char[]Topic;
+	//public char[200] Topic; führt in JAVA zu Compilerfehlern
+	private int Topic_Laenge = 200;
+	public char[] Topic = new char[Topic_Laenge];
 	boolean status = false;
 	int Schaltvorgaenge = 0;
 	int Adresse = 0;
@@ -13,22 +15,31 @@ public class Aktor {
 	{
 		status = true;
 		Schaltvorgaenge++;
+		System.out.println("Aktor wurde eingeschaltet.");
 	}
 	
 	void schalte_aus()
 	{
 		status = false;
+		System.out.println("Aktor wurde eingeschaltet.");
 	}
 	
-	void schalte(boolean einschalten)
+	public void schalte(boolean einschalten)
 	{
-		if(einschalten == true)
+		if(!(einschalten == get_status())) 
 		{
-			schalte_ein();
+			if(einschalten == true)
+			{
+				schalte_ein();
+			}
+			else
+			{
+				schalte_aus();
+			}
 		}
 		else
 		{
-			schalte_aus();
+			System.out.println("Aktor war bereits im angeforderten Status.");
 		}
 	}
 	
@@ -47,5 +58,72 @@ public class Aktor {
 	public boolean get_status()
 	{
 		return status;
+	}
+	
+	public char[] get_topic()
+	{
+		//Sicherstellen, dass das Char Array einen Begrenzer hat, dass die Ausgabe des String auf jeden Fall enden lässt
+		//Topic[199] = '\n';
+		return Topic;
+	}
+	
+	public int get_Topic_Lanege()
+	{
+		return Topic_Laenge;
+	}
+	
+	public int JSON_2_Aktor(char[] JSON_Input)
+	{
+		//Im ersten Teil
+		System.out.println("JSON_2_Aktor:");
+		Topic = JSON_Input;
+		char[][] Param = new char[10][30];
+		int[] quot_marks_pos = search_for_next_two_quotation_marks(0);
+		for(int i=0; i<(quot_marks_pos[1]-quot_marks_pos[0]-1); i++)
+		{
+			Param[0][i] = Topic[i+quot_marks_pos[0]+1];
+			Param[0][i+1] = '\n';
+		}
+		for(int i=0; Param[0][i]!='\n'; i++)
+		{
+			System.out.print(Param[0][i]);
+		}
+		//Im gutfall eine Rückmeldung geben, dass die Transformation geklappt hat
+		return 0;
+	}
+	
+	int[] search_for_next_two_quotation_marks(int start_position)
+	{
+		int[] quotation_mark_position = { 0 , 0};
+		int j=0;
+		for(int i=start_position; Topic[i]!='\n'; i++)
+		{
+			if (j<2)
+			{
+				if(Topic[i]=='"') //wenn Anführungszeichen erkannt wurden
+				{
+					quotation_mark_position[j] = i;
+					j++;
+				}
+				
+			}
+		}
+		return quotation_mark_position;
+	}
+	
+	char[] String_between_quotation_marks(char[] String_with_2quot_marks)
+	{
+		char[] Param = new char[30];
+		int[] quot_marks_pos = search_for_next_two_quotation_marks(0);
+		for(int i=0; i<(quot_marks_pos[1]-quot_marks_pos[0]-1); i++)
+		{
+			Param[i] = String_with_2quot_marks[i+quot_marks_pos[0]+1];
+			Param[i+1] = '\n';
+		}
+		for(int i=0; Param[i]!='\n'; i++)
+		{
+			System.out.print(Param[i]);
+		}
+		return Param;
 	}
 }
